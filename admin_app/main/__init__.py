beee03e3eb9fe1910529dfd71ec222bff7888761
justice ndou <mobiusndou@gmail.com>
2021-04-29 12:43:04 +0200

    memberships updated

--- admin_app/main/__init__.py
+++ admin_app/main/__init__.py
@@ -28,6 +28,7 @@ def create_admin_app(config_class=Config):
     from ..users.routes import users_bp
     from ..helpdesk.routes import helpdesk_bp
     from ..uploads.routes import uploads_bp
+    from ..memberships.routes import memberships_bp
 
     # blue prints
     admin_app.register_blueprint(admin_bp)
@@ -36,5 +37,6 @@ def create_admin_app(config_class=Config):
     admin_app.register_blueprint(users_bp)
     admin_app.register_blueprint(helpdesk_bp)
     admin_app.register_blueprint(uploads_bp)
+    admin_app.register_blueprint(memberships_bp)
 
     return admin_app
--- admin_app/memberships/routes.py
+++ admin_app/memberships/routes.py
@@ -9,16 +9,20 @@ def membership_plans() -> tuple:
     if request.method == "GET":
         # TODO fetch membership plans from data-store
         response = api_fetcher.get_all_membership_plans()
-        status, payload, message = response
-        if status is True:
+        try:
+            status, payload, message = response
             membership_plans_list: typing.List[dict] = payload
-        else:
+        except Exception:
+            status, message = response
             membership_plans_list: typing.List[dict] = []
 
         return render_template('memberships/membership_plans.html',  membership_plans=membership_plans_list)
+
+
+@memberships_bp.route('/create-membership_plan', methods=['GET', 'POST'])
+def create_membership_plan() -> tuple:
+    if request.method == "GET":
+        return render_template('memberships/create_plans.html')
     elif request.method == "POST":
         membership_plan_data: dict = request.get_json()
         return api_sender.send_membership_plans(membership_plan=membership_plan_data)
-
-
-
