852ba6103d6cdf8a0a4ab86f23842d9888972251
mobius-crypt <mobiusndou@gmail.com>
2021-06-21 15:52:38 +0200

    bug: resolved iterable issue

--- admin_app/memberships/routes.py
+++ admin_app/memberships/routes.py
@@ -1,5 +1,5 @@
 import typing
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, jsonify
 from admin_app.main import api_sender, api_fetcher
 
 memberships_bp = Blueprint('memberships', __name__)
@@ -17,15 +17,16 @@ def membership_plans() -> tuple:
             status, message = response
             membership_plans_list: typing.List[dict] = []
 
-        return render_template('memberships/membership_plans.html', membership_plans=membership_plans_list)
+        return render_template('memberships/membership_plans.html', membership_plans=membership_plans_list), 200
 
 
 @memberships_bp.route('/create-membership_plan', methods=['GET', 'POST'])
 def create_membership_plan() -> tuple:
     import asyncio
     if request.method == "GET":
-        return render_template('memberships/create_plans.html')
+        return render_template('memberships/create_plans.html'), 200
     elif request.method == "POST":
         membership_plan_data: dict = request.get_json()
         loop = asyncio.new_event_loop()
-        return  loop.run_until_complete(asyncio.wait(api_sender.send_membership_plans(membership_plan=membership_plan_data)))
+        loop.run_until_complete(asyncio.wait([api_sender.send_membership_plans(membership_plan=membership_plan_data)]))
+        return jsonify({'status': True, 'message': 'membership plan successfully sent'}), 200
